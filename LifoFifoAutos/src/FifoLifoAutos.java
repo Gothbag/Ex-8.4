f872164c6bb4ec98f07902cbbd792e6d4bffb5d4
Gothbag <josemariacagigalso@gmail.com>
2015-09-27 20:35:11 +0200

    Modified main file

--- LifoFifoAutos/src/FifoLifoAutos.java
+++ LifoFifoAutos/src/FifoLifoAutos.java
@@ -4,18 +4,16 @@
  * 
  */
 
-import java.util.List;
 import java.util.LinkedList;
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class FifoLifoAutos {
 	
-	public static void printCars(LinkedList<Car> pCars) {
+	public static void printRemoveCars(LinkedList<Car> pCars) {
 		for (Car c : pCars) {
 			c.setFiscalHorsepower(0);
 			System.out.println(c.getMake() + " " + c.getModel() + " " + c.getEngineDisplacement() + " " + c.getNCylinders());
-			
+			pCars.remove(c);
 		}
 		
 	}
@@ -33,20 +31,29 @@ public class FifoLifoAutos {
 		Car focci = new Car("Ford", "Focus", 2000, 4);
 		//the Ford Focus is appended to the list
 		cars.addLast(focci);
+		
+		//1
 		LinkedList<Car> lifoAutos = new LinkedList<Car>(cars);
 		LinkedList<Car> fifoAutos = new LinkedList<Car>(cars);
+		
+		//3
 		Car pandi = new Car("Fiat", "Panda", 900, 2);
 		cars.offer(pandi);
 		lifoAutos.offer(pandi);
 		fifoAutos.offer(pandi);
-		//now we go over the array to print out the details of each car in it.
-		System.out.println("A list");
-		printCars(cars);
-		System.out.println("Another list");
-		printCars(lifoAutos);
-		System.out.println("And yet another list");
-		printCars(fifoAutos);
-		PriorityQueue<Car> priorCars = null;
+		//2
+		PriorityQueue<Car> priorCars = new PriorityQueue<>(cars);
+		
+		//4
+		//now we loop over the first three lists and remove every car from each as we print out the details of them all
+		printRemoveCars(lifoAutos);
+		printRemoveCars(fifoAutos);
+		printRemoveCars(cars);
+		//5
+		//An example in real life of a LinkedList would be a group of people standing in an unorderly fashion waiting to be
+		//served at a shop. a person standing in any given spot might be served earlier than if people were standing in a
+		//line but it's harder to determine for the clerk who's first and who's last
+		//A real-life example of a PriorityQueue is a regular, orderly queue at a shop
 	}
 
 }
